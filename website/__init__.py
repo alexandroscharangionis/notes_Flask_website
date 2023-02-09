076d45eab742293bbb6c88ce4c561c53854b9df7
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-02-09 18:18:39 +0200

    added blueprint routes to Flask app init function

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,12 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "12345"
+
+    # Import blueprints with routes and register them to Flask app
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
     return app
