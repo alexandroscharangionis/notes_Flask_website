e4c016f80d971e126c33fca87fab82fae7804dda
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-02-09 18:36:00 +0200

    initial routes for testing

--- website/auth.py
+++ website/auth.py
@@ -2,3 +2,18 @@ from flask import Blueprint
 
 # Set up blueprint variable for Flask app (actual blueprint is called the same as the variable it's contained in)
 auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login")
+def login():
+    return "<p>Login</p>"
+
+
+@auth.route("/logout")
+def logout():
+    return "<p>Logout</p>"
+
+
+@auth.route("/sign-up")
+def sign_up():
+    return "<p>Sign up</p>"
