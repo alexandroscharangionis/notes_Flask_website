675813cad78a25c4bb1ec320d474a354cce76be8
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-02-09 18:14:39 +0200

    homepage route for testing

--- website/views.py
+++ website/views.py
@@ -2,3 +2,8 @@ from flask import Blueprint
 
 # Set up blueprint variable for Flask app (actual blueprint is called the same as the variable it's contained in)
 views = Blueprint("views", __name__)
+
+
+@views.route("/")
+def home():
+    return "<h1>Test</h1>"
