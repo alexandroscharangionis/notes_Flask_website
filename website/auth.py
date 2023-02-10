9d1d01cc23aef012ddef4743491a5e396c6ce496
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-02-10 17:02:46 +0200

    basic logic for signup form validation

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 # Set up blueprint variable for Flask app (actual blueprint is called the same as the variable it's contained in)
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["get", "post"])
 def login():
     return render_template("login.html")
 
@@ -14,6 +14,23 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=["get", "post"])
 def sign_up():
+    if request.method == "post":
+        email = request.form.get("email")
+        first_name = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(email) < 4:
+            pass
+        elif len(first_name) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            # add user to database
+            pass
     return render_template("signup.html")
