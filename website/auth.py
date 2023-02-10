7d43ff6c388ea51a5cddc75d8be4f6e4b771e60e
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-02-10 17:47:10 +0200

    typo

--- website/auth.py
+++ website/auth.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template, request, flash
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login", methods=["get", "post"])
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
@@ -14,9 +14,9 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route("/sign-up", methods=["get", "post"])
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
-    if request.method == "post":
+    if request.method == "POST":
         email = request.form.get("email")
         first_name = request.form.get("firstName")
         password1 = request.form.get("password1")
