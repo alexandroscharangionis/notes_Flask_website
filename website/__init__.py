476ada6a5258db0fa4aa2435fa7c14bf40082736
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-02-11 15:52:59 +0200

    SQLAlchemy databse basic setup

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
-# Initialize flask app, add secret key to config and return app
+# Initialize flask app, add secret key to config, add SQL relative path to config, register route blueprints and return app
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "12345"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     # Import blueprints with routes and register them to Flask app
     from .views import views
