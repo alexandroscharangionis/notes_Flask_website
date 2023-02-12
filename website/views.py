eb8fbd2911165ecf262eb7530e56508d6931fd73
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-02-12 16:55:33 +0200

    note creation functionality

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
 
 
 # Set up blueprint variable for Flask app (actual blueprint is called the same as the variable it's contained in)
@@ -15,6 +17,9 @@ def home():
         if len(note) < 1:
             flash("Note is too short.", category="error")
         else:
+            new_note = Note(text=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
             flash("Note added.", category="success")
 
             # 'user' arg references the current user via the current_user method inside the html template, so we can display some html elements only if user is signed-in
