b93d61485e22859bd15a1fd9a69164c575ab87e9
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-02-12 16:24:01 +0200

    variable typo

--- website/auth.py
+++ website/auth.py
@@ -66,7 +66,7 @@ def sign_up():
                             password=generate_password_hash(password1, method="sha256"))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash("Account created successfully!", category="success")
             return redirect(url_for("views.home"))
 
