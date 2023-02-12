10872e0ba204c54c85c1614572ff5fed1dbf53a2
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-02-12 15:38:41 +0200

    added decorator

--- website/views.py
+++ website/views.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
 
 # Set up blueprint variable for Flask app (actual blueprint is called the same as the variable it's contained in)
 views = Blueprint("views", __name__)
 
 
 @views.route("/")
+# Decorator that prevents accessing homepage unless logged in
+@login_required
 def home():
     return render_template("home.html")
