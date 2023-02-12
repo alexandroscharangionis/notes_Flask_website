7e3c5a7546cd5c05b7f508e026922e60eaf1ed2e
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-02-12 15:58:21 +0200

    login_manager module use + logic for loading user

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -22,5 +23,14 @@ def create_app():
     from .models import User, Note
     with app.app_context():
         db.create_all()
+
+    login_manager = LoginManager()
+    # Redirects user to specified route if login is required:
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
