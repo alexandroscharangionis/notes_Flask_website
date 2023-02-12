4d4057bab116549bdb9ad3fbc9a57f1a12ddfde8
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-02-12 16:43:35 +0200

    note creation error/success messages

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
 
 
@@ -6,9 +6,16 @@ from flask_login import login_required, current_user
 views = Blueprint("views", __name__)
 
 
-@views.route("/")
+@views.route("/", methods=["GET", "POST"])
 # Decorator that prevents accessing homepage unless logged in
 @login_required
 def home():
-    # 'user' arg references the current user via the current_user method inside the html template, so we can display some html elements only if user is signed-in
+    if request.method == "POST":
+        note = request.form.get("note")
+        if len(note) < 1:
+            flash("Note is too short.", category="error")
+        else:
+            flash("Note added.", category="success")
+
+            # 'user' arg references the current user via the current_user method inside the html template, so we can display some html elements only if user is signed-in
     return render_template("home.html", user=current_user)
